ffd246cb35f432eda93a2cd548abc88ad9c8e0ee
Brandon Devine <brandon.devine@gmail.com>
2014-05-29 15:35:07 -0700

    Added some more auth stuff.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask,
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.login import LoginManager
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -12,18 +13,27 @@ mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'     # auth is the blueprint name defined in auth/__init__.py
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
-    db.init_app(app)
+
 
     from .main import main as main_blueprint    # .main is defined in main/__init__.py
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint    # .auth is likewise defined in app/auth/__init__.py
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
 
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask.ext.login import UserMixin
+from . import db, login_manager
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -12,15 +13,13 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
     @property
     def password(self):
@@ -32,3 +31,10 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
